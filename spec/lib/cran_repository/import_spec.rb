5462599c7cc7a4264d831c1b592eb66007e16000
Raphael Costa <raphael@raphaelcosta.net>
2014-12-11 03:00:05 -0200

    dont import packages that app already have

--- spec/lib/cran_repository/import_spec.rb
+++ spec/lib/cran_repository/import_spec.rb
@@ -23,12 +23,23 @@ describe CranRepository::Import do
 
         first_package = Package.first
         expect(first_package.name).to eq('A3')
+        expect(first_package.version).to eq('0.9.2')
         expect(first_package.title).to eq('A3: Accurate, Adaptable, and Accessible Error Metrics for Predictive Models')
         expect(first_package.authors).to eq(['Scott Fortmann-Roe'])
         expect(first_package.date_publication).to eq('2013-03-26 19:58:40')
         expect(first_package.maintainer_email).to eq('scottfr@berkeley.edu')
         expect(first_package.maintainer_name).to eq('Scott Fortmann-Roe')
         expect(first_package.description).to eq('This package supplies tools for tabulating and analyzing the results of predictive models. The methods employed are applicable to virtually any predictive model and make comparisons between different methodologies straightforward.')
+      end
+    end
+
+    context 'when we already have imported one of the repositories' do
+      before do
+        Package.create(name: 'A3', version:  '0.9.2')
+      end
+
+      it 'must import only 2 new packages' do
+        expect{ CranRepository::Import.call }.to change{Package.count}.by(2)
       end
     end
   end
