968072ba3ae9b3abc9dfb3b6e80493224182e3ad
Raphael Costa <raphael@raphaelcosta.net>
2014-12-10 22:35:56 -0200

    add rspec for tests

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require File.expand_path('../boot', __FILE__)
 require "active_model/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
-require "action_mailer/railtie"
+# require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
@@ -18,6 +18,8 @@ module CranRPackagesWeb
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
